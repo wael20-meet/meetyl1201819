38ccf7bccf3a9ad2f127d1042bd08ef13ecf44ec
wael20-meet <wael20@meet.mit.edu>
2018-10-17 19:23:19 +0300

    my first commit

---
+++ lab1.py
@@ -0,0 +1,39 @@
+import turtle
+
+turtle.pensize(25)
+turtle.color("blue")
+turtle.penup()
+turtle.goto(-110, -25)
+turtle.pendown()
+turtle.circle(100)
+
+turtle.pensize(25)
+turtle.color("black")
+turtle.penup()
+turtle.goto(100, -25)
+turtle.pendown()
+turtle.circle(100)
+
+turtle.pensize(25)
+turtle.color("red")
+turtle.penup()
+turtle.goto(310, -25)
+turtle.pendown()
+turtle.circle(100)
+
+turtle.pensize(25)
+turtle.pensize(25)
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(-55, -75)
+turtle.pendown()
+turtle.circle(100)
+
+turtle.pensize(25)
+turtle.color("green")
+turtle.penup()
+turtle.goto(55, -75)
+turtle.pendown()
+turtle.circle(100)
+
+turtle.done()
---
+++ lab2.py
@@ -0,0 +1,20 @@
+def exercise01(x):
+	z = x[0]
+	y = x[4]
+	list01 = [z,y]
+	return list01
+
+
+list02 = [1,2,3,4,5]
+
+exercise01 (list02)
+list01 = exercise01(list02)
+print(list01)
+
+c = [1,2,4,3,5,23,29,55,68,79]
+
+def ex02(l):
+	for i in l:
+		if i < 5:  
+			print(i)
+ex02(c)
